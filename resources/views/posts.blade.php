05174e9b2570e442e6296813a7b8e5ab3f38a114
Dana Sugu <dana.sugu@gmail.com>
2021-03-24 21:01:25 +0200

    create & edit posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               <span>all your posts go here</span>
+               <!-- @foreach ($posts as $post )
+                <div class="p-4 border-b border-gray-200">
+                <h2 class="text-xl font-medium"> {{ $post->title }} </h2>
+                <p class="text gray-600 leading-relaxed"> {{ $post->body }} </p>  
+                </div>    
+               @endforeach -->
+
+               @livewire('posts', ['posts' => $posts])
             </div>
         </div>
     </div>
